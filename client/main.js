c2ee96aa86364810ae4c45d82c295e04cbfe360d
ProSavage <naman0311@gmail.com>
2019-02-10 01:49:39 -0500

    Rank sorting by score!

--- client/main.js
+++ client/main.js
@@ -8,9 +8,7 @@ import App from "../imports/ui/App";
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
-
-
-        let players = Players.find().fetch();
+        let players = Players.find({}, {sort: {score: -1}}).fetch();
         let title = "Score Keep";
         ReactDOM.render(<App players={players} title={title}/>, document.getElementById("app"));
 
